9197a22f4578c6d969a681001515e04de5425714
Valeria Radchenko <81882028+leracherry@users.noreply.github.com>
2024-12-02 03:56:25 +0100

    Fix header routing

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box } from '@mui/material';
-import { Link, useLocation } from 'react-router-dom';
+import {AppBar, Toolbar, Typography, Box} from '@mui/material';
+import {Link, useLocation} from 'react-router-dom';
 
 const activeFontColor = '#f3bac3';
 
@@ -9,22 +9,22 @@ const Header = () => {
 
     return (
         <AppBar position="sticky">
-            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
                 <Box display="flex" alignItems="center">
                     <img
                         src="/gary.webp"
                         alt="Custom Icon"
-                        style={{ width: 50, height: 50, marginRight: 10 }}
+                        style={{width: 50, height: 50, marginRight: 10}}
                     />
                     <Typography variant="h6">Welbi</Typography>
                 </Box>
-                <Box sx={{ flexGrow: 1, display: 'flex', paddingRight: '70px', justifyContent: 'center' }}>
+                <Box sx={{flexGrow: 1, display: 'flex', paddingRight: '70px', justifyContent: 'center'}}>
                     <Typography
                         variant="h6"
                         component={Link}
                         to="/residents"
                         sx={{
-                            color: location.pathname === '/residents' ? activeFontColor : 'inherit',  // Check if active tab
+                            color: location.pathname === '/residents' || location.pathname === '/' ? activeFontColor : 'inherit',  // Check if active tab
                             textDecoration: 'none',
                             margin: '0 20px',
                             '&:hover': {
